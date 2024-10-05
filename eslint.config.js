afc344f9b2dec2e7f7ae49a565625a0a215ff49a
bassem-krayem <bassem.krayem09@gmail.com>
2024-10-05 11:25:46 +0100

    preparing the files and project structurre

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+    {
+      files: ["**/*.js"],
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+      rules: {
+        "no-unused-vars": "warn",
+        "no-console": "off",
+      },
+    },
+  ];
+  
